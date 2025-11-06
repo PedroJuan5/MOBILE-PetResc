8be12cc09554e8f598b7e57e214c2819c589eb54
PedroJuan5 <pd.juan05@gmail.com>
2025-11-06 10:24:22 -0300

    Refactor pet adoption screens and navigation

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,10 +1,10 @@
-
 import { AuthProvider, useAuth } from '@/context/AuthContext';
-import { Slot, SplashScreen, useRouter, useSegments } from 'expo-router';
+import { Stack, SplashScreen, useRouter, useSegments } from 'expo-router';
 import React, { useEffect } from 'react';
 
 SplashScreen.preventAutoHideAsync();
 
+// Esta é a sua lógica de navegação e autenticação
 function RootLayoutNav() {
   const { user, isLoading } = useAuth();
   const segments = useSegments();
@@ -15,32 +15,38 @@ function RootLayoutNav() {
       const inAuthGroup = segments[0] === '(auth)';
 
       if (user && inAuthGroup) {
-         
         SplashScreen.hideAsync();
         router.replace('/(tabs)/home' as any);
       } else if (!user && !inAuthGroup) {
-        console.log("Chegou aqui")
-        console.log(user)
-        console.log(inAuthGroup)
+        console.log("Chegou aqui");
+        console.log(user);
+        console.log(inAuthGroup);
         SplashScreen.hideAsync();
-        router.replace('/' as any);
+        router.replace('/' as any); // Ou router.replace('/(auth)');
       } else {
-         SplashScreen.hideAsync();
+        SplashScreen.hideAsync();
       }
     }
   }, [user, isLoading, segments]);
 
   if (isLoading) {
-    return null;
+    return null; // A SplashScreen continua visível
   }
 
-  return <Slot />;
+  return (
+    <Stack
+      screenOptions={{
+        // Esconde o cabeçalho azul padrão do Stack
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="(tabs)" />
+    </Stack>
+  );
 }
 
-export default function RootLayout() {
+export default function AppGroupLayout() {
   return (
-    <AuthProvider>
-      <RootLayoutNav />
-    </AuthProvider>
+    <RootLayoutNav />
   );
 }
